301a553f316fc8d5b2f57b6747c8a4a1167d5ddd
Ross Fleming <rossfleming2711@gmail.com>
2019-01-06 20:26:15 +0100

    blog pages updated. components added. images added.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
+import { BlogComponent } from './blog/blog.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
+  { path: 'blog', component: BlogComponent },
   { path: 'contact', component: ContactComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,8 +11,15 @@ import { BannerComponent } from './components/banner/banner.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { AuthorComponent } from './components/author/author.component';
-// import { MatCardModule } from '@angular/material';
-
+import { BlogsComponent } from './components/blogs/blogs.component';
+import { SearchComponent } from './components/search/search.component';
+import { MatFormFieldModule, MatInputModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BlogComponent } from './blog/blog.component';
+import { PostComponent } from './components/post/post.component';
+import { PaginationComponent } from './components/pagination/pagination.component';
+import { LatestPostsComponent } from './components/latest-posts/latest-posts.component';
+import { CategoriesComponent } from './components/categories/categories.component';
 
 @NgModule({
   declarations: [
@@ -23,13 +30,22 @@ import { AuthorComponent } from './components/author/author.component';
     BannerComponent,
     AboutComponent,
     ContactComponent,
-    AuthorComponent
+    AuthorComponent,
+    BlogsComponent,
+    SearchComponent,
+    BlogComponent,
+    PostComponent,
+    PaginationComponent,
+    LatestPostsComponent,
+    CategoriesComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
-    // MatCardModule
+    NgbModule,
+    MatFormFieldModule,
+    MatInputModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
